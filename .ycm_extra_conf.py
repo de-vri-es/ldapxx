c540a827953de7121dc65ccf6f3b6ac875f1f903
Maarten de Vries <maarten@de-vri.es>
2017-03-23 23:09:15 +0100

    Add YouCompleteMe configuration.

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,23 @@
+import os
+from glob import glob
+#import ycm_core
+
+base = os.path.dirname(os.path.abspath(__file__))
+
+def FlagsForFile(filename, **kwargs):
+	filedir = os.path.dirname(filename)
+	flags = [
+		'-x', 'c++',
+		'-Wall',
+		'-Wextra',
+		'-Wpedantic',
+		'-fPIC',
+		'-std=c++14',
+		'-isystem', os.path.join(base, 'include/ldapxx'),
+	]
+
+	return {
+		'flags': flags,
+		'do_cache': True
+	}
+
